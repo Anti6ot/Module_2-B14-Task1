a0841e5bc970ccc011b7651d91d27711da47effe
Nikola_Antonov <Android2.1666@email.com>
2023-02-02 14:13:15 +0500

    author task this redact

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -2,74 +2,77 @@ import React, {useState} from "react";
 import api from '../api';
 
 const Users = () => {
-    const fetchUsers = api.users.fetchAll()
-    const [users, setUsers] = useState(fetchUsers)
+    const [users, setUsers] = useState(api.users.fetchAll());
 
     const handleDelete = (userId) => {
 
-        setUsers(prevState => prevState.filter(user => user._id !== userId))
-    }
+        setUsers(users.filter((user) => user._id !== userId))
 
+    }
 
     const renderPhrase = (number) => {
-
-        if(number === 1 || number > 4){
-            return  <span className="badge bg-primary">{number} человек тусанет с тобой сегодня</span>
-        }
-        if (number === 2 || number === 3 || number === 4){
-            return  <span className="badge bg-primary">{number} человека тусанет с тобой сегодня</span>
-        }
-        if(number === 0) {
-            return <span className="badge bg-danger">никто не тусанет с тобой сегодня</span>
-        }
+        const lastOne = Number(number.toString().slice(-1));
+        if (number > 4 && number < 15) return "человек тусанет";
+        if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут";
+        if (lastOne === 1) return "человек тусанет";
+        return "человек тусанет";
     }
 
-    const renderUsers = () => {
-       return  users.map(user => {
-            return <tr
-                key = {user._id}
-            >
-                <td> {user.name} </td>
-                <td> {qualitiesRender(user.qualities)} </td>
-                <td> {user.profession.name} </td>
-                <td> {user.completedMeetings} </td>
-                <td> {user.rate} </td>
-                <td><button className="btn btn-danger" onClick={() => handleDelete(user._id)}> delete </button></td>
-            </tr>
-        })
-    }
 
-    const qualitiesRender = (id) => {
+    return (
+        <>
+            <h2>
+                <span
+                    className={"badge " + (users.length > 0 ? "bg-primary" : "bg-danger")}
+                >
+                    {users.length > 0
+                        ? `${users.length + " " + renderPhrase(users.length)} с тобой сегодня`
+                        : "Никто с тобой не тусанет"}
+                </span>
+            </h2>
 
-        const qualitiesArr = id.map(
-        el => {
-            let classColor = `badge bg-${el.color}`
-           return <span
-               key={el._id}
-               className ={classColor}>
-               {el.name}
-           </span>
-        })
-        return qualitiesArr
-    }
+            {users.length > 0 && (
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">Имя</th>
+                        <th scope="col">Качества</th>
+                        <th scope="col">Профессия</th>
+                        <th scope="col">Встретился, раз</th>
+                        <th scope="col">Оценка</th>
+                        <th />
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {users.map((user) => (
+                        <tr key={user._id}>
+                            <td>{user.name}</td>
+                            <td>
+                                {user.qualities.map((item) => (
+                                    <span className={"badge m-1 bg-" + item.color} key={item._id}>
+                                        {item.name}
+                                    </span>
+                                ))}
+                            </td>
+                            <td>{user.profession.name}</td>
+                            <td>{user.completedMeetings}</td>
+                            <td>{user.rate} /5</td>
+                            <td>
+                                <button
+                                    onClick={() => handleDelete(user._id)}
+                                    className="btn btn-danger"
+                                >
+                                    delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            )}
+        </>
+    );
 
-    return <React.Fragment>
-        <h1>{renderPhrase(users.length)}</h1>
-        <table className="table">
-        <thead>
-        <tr>
-            <th scope="col">Имя</th>
-            <th scope="col">Качества</th>
-            <th scope="col">Профессия</th>
-            <th scope="col">Встретился, раз</th>
-            <th scope="col">Оценка</th>
-        </tr>
-        </thead>
-        <tbody>
-        {renderUsers()}
-        </tbody>
-        </table>
-        </React.Fragment>
 
 };
 export default Users;
