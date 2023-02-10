86700471450d27f6ddf48599fd15b25e397f789e
Nikola_Antonov <Android2.1666@email.com>
2023-02-10 18:17:13 +0500

    first commit

--- src/app/components/bookmark.jsx
+++ src/app/components/bookmark.jsx
@@ -1,17 +1,14 @@
-import React, {useState} from "react";
-import api from "../api";
 
-// const Bookmark = (id) => {
-//     const [users, setUsers] = useState(api.users.fetchAll());
-//
-//     setUsers(users.map(el => {
-//         if(el._id === id && !el.bookmark){
-//             el.bookmark = true
-//         } else if (el._id === id && el.bookmark){
-//             el.bookmark = false
-//         }
-//         return el
-//     }))
-// }
+const Bookmark = (id, users) => {
 
-// export default Bookmark
+    return (users.map(el => {
+        if(el._id === id && !el.bookmark){
+            el.bookmark = true
+        } else if (el._id === id && el.bookmark){
+            el.bookmark = false
+        }
+        return el
+    }))
+}
+
+export default Bookmark
