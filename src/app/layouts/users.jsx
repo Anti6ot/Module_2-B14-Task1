93e57fa9a33d4895bf5fbd86f8af6bf1c8251d10
Nikola_Antonov <Android2.1666@email.com>
2023-05-04 17:51:12 +0500

    Вызов серверных служб (API), кастомные хуки и управление состоянием

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -3,10 +3,26 @@ import { useParams } from "react-router-dom";
 import UserEdit from "../components/page/userEdit/userEdit";
 import UserPage from "../components/page/userPage";
 import UsersListPage from "../components/page/usersListPage";
+
+import UserProvider from "../hooks/useUsers";
 const Users = () => {
-    const params = useParams();
-    const { userId, edit } = params;
-    return <>{userId ? (edit ? <UserEdit/> : <UserPage userId={userId} />) : <UsersListPage />}</>;
+  const params = useParams();
+  const { userId, edit } = params;
+  return (
+    <>
+      <UserProvider>
+        {userId ? (
+          edit ? (
+            <UserEdit />
+          ) : (
+            <UserPage userId={userId} />
+          )
+        ) : (
+          <UsersListPage />
+        )}
+      </UserProvider>
+    </>
+  );
 };
 
 export default Users;
